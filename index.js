395435698ce629b302a848b17bb0aad6650da0a9
Michalis Gerber <128619468+highlordmike@users.noreply.github.com>
2023-07-17 15:47:02 +0100

    update game 2.1

--- index.js
+++ index.js
@@ -1,145 +1,198 @@
-(function(name,data){
-  if(typeof onTileMapLoaded === 'undefined') {
-   if(typeof TileMaps === 'undefined') TileMaps = {};
-   TileMaps[name] = data;
-  } else {
-   onTileMapLoaded(name,data);
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1280
+canvas.height = 768
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const placementTilesData2D = []
+
+for (let i = 0; i < placementTilesData.length; i += 20) {
+  placementTilesData2D.push(placementTilesData.slice(i, i + 20))
+}
+
+const placementTiles = []
+
+placementTilesData2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 14) {
+      // add building placement tile here
+      placementTiles.push(
+        new PlacementTile({
+          position: {
+            x: x * 64,
+            y: y * 64
+          }
+        })
+      )
+    }
+  })
+})
+
+const image = new Image()
+
+image.onload = () => {
+  animate()
+}
+image.src = 'img/gameMap.png'
+
+const enemies = []
+
+function spawnEnemies(spawnCount) {
+  for (let i = 1; i < spawnCount + 1; i++) {
+    const xOffset = i * 150
+    enemies.push(
+      new Enemy({
+        position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
+      })
+    )
   }
-  if(typeof module === 'object' && module && module.exports) {
-   module.exports = data;
-  }})("pp2",
- { "compressionlevel":-1,
-  "height":12,
-  "infinite":false,
-  "layers":[
-         {
-          "data":[16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-             16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16],
-          "height":12,
-          "id":3,
-          "name":"Copy of Copy of Tile Layer 1",
-          "opacity":1,
-          "type":"tilelayer",
-          "visible":true,
-          "width":20,
-          "x":0,
-          "y":0
-         }, 
-         {
-          "data":[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0,
-             15, 16, 16, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 16, 16, 16, 16, 16, 16,
-             15, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 16, 16, 4, 5, 5, 5,
-             15, 16, 16, 18, 19, 22, 33, 33, 33, 33, 21, 19, 19, 20, 16, 16, 18, 19, 19, 19,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 22, 33,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             5, 5, 5, 7, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             19, 19, 19, 19, 19, 20, 16, 16, 16, 16, 18, 19, 19, 8, 5, 5, 7, 19, 20, 16,
-             33, 33, 33, 33, 33, 34, 16, 16, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 20, 16,
-             15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 32, 33, 33, 33, 33, 33, 33, 33, 34, 16,
-             29, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-          "height":12,
-          "id":4,
-          "name":"Copy of Copy of Copy of Tile Layer 1",
-          "opacity":1,
-          "type":"tilelayer",
-          "visible":true,
-          "width":20,
-          "x":0,
-          "y":0
-         }, 
-         {
-          "data":[49, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0,
-             48, 16, 16, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 16, 16, 16, 16, 16, 16,
-             15, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 16, 16, 4, 5, 5, 5,
-             15, 16, 16, 18, 19, 22, 33, 33, 33, 33, 21, 19, 19, 20, 16, 16, 18, 19, 19, 19,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 22, 33,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             5, 5, 5, 7, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             19, 19, 19, 19, 19, 20, 16, 16, 16, 16, 18, 19, 19, 8, 5, 5, 7, 19, 20, 16,
-             33, 33, 33, 33, 33, 34, 16, 16, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 20, 16,
-             15, 16, 127, 16, 16, 16, 16, 16, 16, 16, 32, 33, 33, 33, 33, 33, 33, 33, 34, 16,
-             29, 30, 141, 142, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-          "height":12,
-          "id":6,
-          "name":"2",
-          "opacity":1,
-          "type":"tilelayer",
-          "visible":true,
-          "width":20,
-          "x":0,
-          "y":0
-         }, 
-         {
-          "data":[49, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 113, 114, 115,
-             57, 16, 16, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 16, 16, 16, 127, 128, 129,
-             71, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 16, 16, 4, 141, 142, 143,
-             15, 16, 16, 18, 19, 22, 33, 33, 33, 33, 21, 19, 19, 20, 16, 16, 18, 19, 19, 19,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 22, 33,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             5, 5, 5, 7, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             19, 19, 19, 19, 19, 20, 16, 16, 16, 16, 18, 19, 19, 8, 5, 5, 7, 19, 20, 16,
-             33, 33, 113, 114, 115, 34, 16, 16, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 20, 16,
-             119, 120, 121, 128, 129, 16, 16, 16, 16, 16, 32, 33, 33, 33, 33, 33, 33, 33, 34, 16,
-             133, 134, 135, 142, 143, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-          "height":12,
-          "id":8,
-          "name":"Copy of 2",
-          "opacity":1,
-          "type":"tilelayer",
-          "visible":true,
-          "width":20,
-          "x":0,
-          "y":0
-         }, 
-         {
-          "data":[49, 2, 2, 2, 2, 2, 2, 2, 2, 2, 47, 2, 2, 2, 2, 2, 0, 113, 114, 115,
-             57, 16, 16, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 16, 16, 16, 127, 128, 129,
-             71, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 16, 16, 4, 141, 142, 143,
-             15, 16, 16, 18, 19, 22, 33, 33, 33, 33, 21, 19, 19, 20, 16, 16, 18, 19, 19, 19,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 22, 47,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             15, 16, 16, 18, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             5, 5, 5, 7, 19, 20, 16, 16, 16, 16, 18, 19, 19, 20, 16, 16, 18, 19, 20, 16,
-             19, 19, 19, 19, 19, 20, 16, 16, 16, 16, 18, 19, 19, 8, 5, 5, 7, 19, 20, 16,
-             33, 33, 113, 114, 115, 34, 16, 16, 16, 16, 18, 19, 19, 19, 19, 19, 19, 19, 20, 16,
-             119, 47, 121, 128, 129, 16, 16, 16, 16, 16, 32, 33, 33, 33, 33, 33, 33, 33, 34, 16,
-             133, 134, 135, 142, 143, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-          "height":12,
-          "id":9,
-          "name":"Copy of Copy of 2",
-          "opacity":1,
-          "type":"tilelayer",
-          "visible":true,
-          "width":20,
-          "x":0,
-          "y":0
-         }],
-  "nextlayerid":11,
-  "nextobjectid":1,
-  "orientation":"orthogonal",
-  "renderorder":"right-down",
-  "tiledversion":"1.10.1",
-  "tileheight":64,
-  "tilesets":[
-         {
-          "firstgid":1,
-          "source":"..\/..\/..\/tileset.tsx"
-         }],
-  "tilewidth":64,
-  "type":"map",
-  "version":"1.10",
-  "width":20
- });
+}
+
+const buildings = []
+let activeTile = undefined
+let enemyCount = 3
+let hearts = 10
+let coins = 100
+const explosions = []
+spawnEnemies(enemyCount)
+
+function animate() {
+  const animationId = requestAnimationFrame(animate)
+
+  c.drawImage(image, 0, 0)
+
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i]
+    enemy.update()
+
+    if (enemy.position.x > canvas.width) {
+      hearts -= 1
+      enemies.splice(i, 1)
+      document.querySelector('#hearts').innerHTML = hearts
+
+      if (hearts === 0) {
+        console.log('game over')
+        cancelAnimationFrame(animationId)
+        document.querySelector('#gameOver').style.display = 'flex'
+      }
+    }
+  }
+
+  for (let i = explosions.length - 1; i >= 0; i--) {
+    const explosion = explosions[i]
+    explosion.draw()
+    explosion.update()
+
+    if (explosion.frames.current >= explosion.frames.max - 1) {
+      explosions.splice(i, 1)
+    }
+
+    console.log(explosions)
+  }
+
+  // tracking total amount of enemies
+  if (enemies.length === 0) {
+    enemyCount += 2
+    spawnEnemies(enemyCount)
+  }
+
+  placementTiles.forEach((tile) => {
+    tile.update(mouse)
+  })
+
+  buildings.forEach((building) => {
+    building.update()
+    building.target = null
+    const validEnemies = enemies.filter((enemy) => {
+      const xDifference = enemy.center.x - building.center.x
+      const yDifference = enemy.center.y - building.center.y
+      const distance = Math.hypot(xDifference, yDifference)
+      return distance < enemy.radius + building.radius
+    })
+    building.target = validEnemies[0]
+
+    for (let i = building.projectiles.length - 1; i >= 0; i--) {
+      const projectile = building.projectiles[i]
+
+      projectile.update()
+
+      const xDifference = projectile.enemy.center.x - projectile.position.x
+      const yDifference = projectile.enemy.center.y - projectile.position.y
+      const distance = Math.hypot(xDifference, yDifference)
+
+      // this is when a projectile hits an enemy
+      if (distance < projectile.enemy.radius + projectile.radius) {
+        // enemy health and enemy removal
+        projectile.enemy.health -= 20
+        if (projectile.enemy.health <= 0) {
+          const enemyIndex = enemies.findIndex((enemy) => {
+            return projectile.enemy === enemy
+          })
+
+          if (enemyIndex > -1) {
+            enemies.splice(enemyIndex, 1)
+            coins += 25
+            document.querySelector('#coins').innerHTML = coins
+          }
+        }
+
+        console.log(projectile.enemy.health)
+        explosions.push(
+          new Sprite({
+            position: { x: projectile.position.x, y: projectile.position.y },
+            imageSrc: './img/explosion.png',
+            frames: { max: 4 },
+            offset: { x: 0, y: 0 }
+          })
+        )
+        building.projectiles.splice(i, 1)
+      }
+    }
+  })
+}
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+canvas.addEventListener('click', (event) => {
+  if (activeTile && !activeTile.isOccupied && coins - 50 >= 0) {
+    coins -= 50
+    document.querySelector('#coins').innerHTML = coins
+    buildings.push(
+      new Building({
+        position: {
+          x: activeTile.position.x,
+          y: activeTile.position.y
+        }
+      })
+    )
+    activeTile.isOccupied = true
+    buildings.sort((a, b) => {
+      return a.position.y - b.position.y
+    })
+  }
+})
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+
+  activeTile = null
+  for (let i = 0; i < placementTiles.length; i++) {
+    const tile = placementTiles[i]
+    if (
+      mouse.x > tile.position.x &&
+      mouse.x < tile.position.x + tile.size &&
+      mouse.y > tile.position.y &&
+      mouse.y < tile.position.y + tile.size
+    ) {
+      activeTile = tile
+      break
+    }
+  }
+})
