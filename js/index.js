3823621b425ca70fd785ddd8313d9c519abc3573
Michalis Gerber <128619468+highlordmike@users.noreply.github.com>
2023-08-04 17:19:49 +0100

    i have no idea what im doing

---
+++ js/index.js
@@ -0,0 +1,95 @@
+function animate() {
+    const animationId = requestAnimationFrame(animate);
+  
+    c.drawImage(image, 0, 0);
+  
+    for (let i = enemies.length - 1; i >= 0; i--) {
+      const enemy = enemies[i];
+      enemy.update();
+  
+      if (enemy.position.x > canvas.width) {
+        hearts -= 1;
+        enemies.splice(i, 1);
+        document.querySelector('#hearts').innerHTML = hearts;
+  
+        if (hearts === 0) {
+          console.log('game over');
+          cancelAnimationFrame(animationId);
+          document.querySelector('#gameOver').style.display = 'flex';
+        }
+      }
+    }
+  
+    for (let i = explosions.length - 1; i >= 0; i--) {
+      const explosion = explosions[i];
+      explosion.draw();
+      explosion.update();
+  
+      if (explosion.frames.current >= explosion.frames.max - 1) {
+        explosions.splice(i, 1);
+      }
+  
+      console.log(explosions);
+    }
+  
+    // tracking total amount of enemies
+    if (enemies.length === 0) {
+      enemyCount += 2;
+      spawnEnemies(enemyCount);
+    }
+  
+    placementTiles.forEach((tile) => {
+      tile.update(mouse);
+    });
+  
+    buildings.forEach((building) => {
+      building.update();
+      building.target = null;
+      const validEnemies = enemies.filter((enemy) => {
+        const xDifference = enemy.center.x - building.center.x;
+        const yDifference = enemy.center.y - building.center.y;
+        const distance = Math.hypot(xDifference, yDifference);
+        return distance < enemy.radius + building.radius;
+      });
+      building.target = validEnemies[0];
+  
+      for (let i = building.projectiles.length - 1; i >= 0; i--) {
+        const projectile = building.projectiles[i];
+  
+        projectile.update();
+  
+        const xDifference = projectile.enemy.center.x - projectile.position.x;
+        const yDifference = projectile.enemy.center.y - projectile.position.y;
+        const distance = Math.hypot(xDifference, yDifference);
+  
+        // this is when a projectile hits an enemy
+        if (distance < projectile.enemy.radius + projectile.radius) {
+          // enemy health and enemy removal
+          projectile.enemy.health -= 20;
+          if (projectile.enemy.health <= 0) {
+            const enemyIndex = enemies.findIndex((enemy) => {
+              return projectile.enemy === enemy;
+            });
+  
+            if (enemyIndex > -1) {
+              enemies.splice(enemyIndex, 1);
+              coins += 25;
+              document.querySelector('#coins').innerHTML = coins;
+            }
+          }
+  
+          console.log(projectile.enemy.health);
+          explosions.push(
+            new Sprite({
+              position: { x: projectile.position.x, y: projectile.position.y },
+              imageSrc: './img/explosion.png',
+              frames: { max: 4 },
+              offset: { x: 0, y: 0 },
+            })
+          );
+          building.projectiles.splice(i, 1);
+        }
+      }
+    });
+  }
+  
